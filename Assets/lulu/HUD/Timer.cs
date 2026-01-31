097fb88f4a96079ea1c2dc91b872cf2b88898a18
brieux <brieuxdupuis@gmail.com>
2026-02-01 00:48:52 +0100

    add time

--- Assets/lulu/HUD/Timer.cs
+++ Assets/lulu/HUD/Timer.cs
@@ -1,7 +1,74 @@
+using TMPro;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI textMesh;
+    private float currentTime = 0f;
+
     [SerializeField]
-    private TextMesh textMesh;
+    private float COUNTDOWN_START = 30f;
+    
+    private Vector3 originalPosition;
+    private float originalFontSize;
+
+    void Start()
+    {
+        originalPosition = textMesh.transform.localPosition;
+        originalFontSize = textMesh.fontSize;
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if (currentTime < COUNTDOWN_START)
+        {
+            HandleCountdown(COUNTDOWN_START - currentTime);
+        }
+        else
+        {
+            HandleStopwatch(currentTime - COUNTDOWN_START);
+        }
+    }
+
+    private void HandleCountdown(float remainingTime)
+    {
+        int seconds = (int)remainingTime;
+        int milliseconds = (int)((remainingTime - seconds) * 100);
+        textMesh.text = $"{seconds:00}:{milliseconds:00}";
+
+        if (remainingTime <= 5f)
+        {
+            textMesh.color = Color.red;
+
+            float t = 1f - (remainingTime / 5f); 
+            textMesh.fontSize = Mathf.Lerp(originalFontSize, originalFontSize * 1.5f, t);
+
+            float shakeIntensity = t * 5f; 
+            textMesh.transform.localPosition = originalPosition + (Vector3)Random.insideUnitCircle * shakeIntensity;
+        }
+        else
+        {
+            textMesh.color = Color.white;
+            textMesh.fontSize = originalFontSize;
+            textMesh.transform.localPosition = originalPosition;
+        }
+    }
+
+    private void HandleStopwatch(float elapsedTime)
+    {
+        if (textMesh.color != Color.white)
+        {
+            textMesh.color = Color.white;
+            textMesh.fontSize = originalFontSize;
+            textMesh.transform.localPosition = originalPosition;
+        }
+
+        int minutes = (int)(elapsedTime / 60);
+        int seconds = (int)(elapsedTime % 60);
+        int milliseconds = (int)((elapsedTime - (int)elapsedTime) * 100);
+
+        textMesh.text = $"{minutes:00}:{seconds:00}:{milliseconds:00}";
+    }
 }
